ac53b77874e739673ccafb7e6b58bfaaca4ecdc1
John Pavley <john@pavley.com>
2016-01-27 18:56:26 -0500

    making it nice

--- Installed Fonts/GameScene.swift
+++ Installed Fonts/GameScene.swift
@@ -36,6 +36,7 @@ class GameScene: SKScene {
     }
     
     override func didMoveToView(view: SKView) {
+        
         let swipeLeftRecognizer = UISwipeGestureRecognizer(target: self, action: "handleLeftSwipe:")
         swipeLeftRecognizer.direction = .Left
         view.addGestureRecognizer(swipeLeftRecognizer)
@@ -43,6 +44,9 @@ class GameScene: SKScene {
         let swipeRightRecognizer = UISwipeGestureRecognizer(target: self, action: "handleRightSwipe:")
         swipeRightRecognizer.direction = .Right
         view.addGestureRecognizer(swipeRightRecognizer)
+        
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: "handleTap:")
+        view.addGestureRecognizer(tapRecognizer)
 
     }
     
@@ -57,6 +61,11 @@ class GameScene: SKScene {
         showFontFamilyByIndex(fontFamilyIndex)
     }
     
+    func handleTap(recognizer:UIGestureRecognizer) {
+        incrementIndex()
+        showFontFamilyByIndex(fontFamilyIndex)
+    }
+    
     func showFontFamilyByIndex(index: Int) {
         removeAllChildren()
         let fontFamilyName = UIFont.familyNames()[fontFamilyIndex]
@@ -68,8 +77,23 @@ class GameScene: SKScene {
             let label = SKLabelNode(fontNamed: fontName)
             label.text = fontName
             
+            // Clever way to arrange the font names on nicely on the screen
+            // from Raywenderlich.com
+            
+            // - Center the labels horizontally
+            // - Divide the vertical space into a stack of evenly size zones
+            // - Postion each label at the bottom of a zone
+            // - Use the number of labels + 1 as the number of zones
+            // - Use a faction to do this interatively
+            
             let x = size.width / 2
-            let y = (size.height * (CGFloat(i+1))) / (CGFloat(fontNames.count)+1)
+            let yNumerator = size.height * (CGFloat(i+1))
+            let yDemoninator = CGFloat(fontNames.count)+1
+            let y = yNumerator / yDemoninator
+            
+            // uncomment this line to see how it works
+            //print("\(y) = \(yNumerator) ÷ \(yDemoninator)")
+            
             label.position = CGPoint(x: x, y: y)
             
             label.fontSize = 50
@@ -79,14 +103,14 @@ class GameScene: SKScene {
         
         let totalFamilyCount = UIFont.familyNames().count
         
-        let titleLabel = SKLabelNode(text: "Installed Families \(totalFamilyCount)")
+        let titleLabel = SKLabelNode(text: "Installed Font Families \(totalFamilyCount)")
         titleLabel.fontName = "Helvetica"
         titleLabel.position = CGPoint(x: size.width / 2, y: size.height - 40)
         titleLabel.fontColor = UIColor.yellowColor()
         addChild(titleLabel)
         
         
-        let indexLabel = SKLabelNode(text: "Family Index: \(fontFamilyIndex), Fonts: \(fontNames.count)")
+        let indexLabel = SKLabelNode(text: "\(fontFamilyName) Index: \(fontFamilyIndex), Fonts: \(fontNames.count)")
         indexLabel.fontName = "Helvetica"
         indexLabel.position = CGPoint(x: size.width / 2, y: 20)
         indexLabel.fontColor = UIColor.yellowColor()
